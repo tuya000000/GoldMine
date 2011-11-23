d73d8fbf7c10e101c8f6826d0abe6ecbe10de818
tuya000000 <tuya000000@me.com>
2011-11-23 11:04:51 +0800

    Clear todo: TradeRecord table should show a header row.

---
+++ src/ui/DefaultHeaderRenderer.java
@@ -0,0 +1,82 @@
+/*
+ *  Copyright (c) 2008 Nokia Siemens Networks. All rights reserved.
+ *
+ *  Revision History:
+ *
+ *  DATE/AUTHOR         COMMENT
+ *  ---------------------------------------------------------------------
+ *  xx.xx.xxxx/ESe                            
+ */
+package ui;
+
+import java.awt.Component;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.JTableHeader;
+
+/**
+ * @author <a href="mailto:esa.seppala@nsn.com">Esa Seppälä</a>
+ */
+public class DefaultHeaderRenderer extends DefaultTableCellRenderer
+{
+
+    /**
+     * Constructor. Sets horizontal alignment to center and border to
+     * <code>UIManager.getBorder( "TableHeader.cellBorder" )</code>.
+     */
+    public DefaultHeaderRenderer()
+    {
+        setHorizontalAlignment( JLabel.CENTER );
+    }
+
+    /**
+     * Configures the renderer appropriately before drawing. After configuration, <code>this</code> component is
+     * returned and used by <code>JTable</code> to draw the cell header.
+     * 
+     * @param table The <code>JTable</code> that is asking the renderer to draw; can be <code>null</code>.
+     * @param value The value of the cell header to be rendered.
+     * @param isSelected True if the cell header is to be rendered with the selection highlighted; otherwise false. Is
+     *        not used in this implementation
+     * @param hasFocus If true, render cell appropriately. Is not used in this implementation
+     * @param row The row index of the cell being drawn. When drawing the header, the value of <code>row</code> is -1.
+     * @param column The column index of the cell being drawn.
+     * @return Component used for drawing the cell.
+     */
+
+    public Component getTableCellRendererComponent( JTable table, Object value, boolean isSelected, boolean hasFocus,
+                                                    int row, int column )
+    {
+        if( table != null )
+        {
+            JTableHeader header = table.getTableHeader();
+            if( header != null )
+            {
+                // To make iconheader look like original
+                setForeground( header.getForeground() );
+                setBackground( header.getBackground() );
+                setFont( header.getFont() );
+            }
+            setBorder( UIManager.getBorder( "TableHeader.cellBorder" ) );
+        }
+
+        if( value instanceof Icon )
+        {
+            setIcon( ( Icon ) value );
+        }
+        else if( value instanceof ImageIcon )
+        {
+            setIcon( ( ImageIcon ) value );
+        }
+        else
+        {
+            setText( value.toString() );
+        }
+
+        return this;
+    }
+}
--- src/ui/GoldMineWindowView.java
+++ src/ui/GoldMineWindowView.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
+import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
@@ -21,117 +22,129 @@ import net.miginfocom.swing.MigLayout;
 
 /**
  * @author tuya
- * 
  */
-public class GoldMineWindowView extends BaseWindowView implements
-		TableModelListener, ActionListener {
-	/**
+public class GoldMineWindowView extends BaseWindowView implements TableModelListener, ActionListener
+{
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = -6943326077921559372L;
+    private static final long serialVersionUID = -6943326077921559372L;
 
-	private JTable myTable;
-	private TradeRecordTable myTradeRecordTableModel;
+    private JTable myTable;
 
-	private JButton myReadFileButton;
+    private TradeRecordTable myTradeRecordTableModel;
 
-	public GoldMineWindowView() {
-		super();
-	}
+    private JButton myReadFileButton;
 
-	/**
-	 * 
-	 * @return <code>true</code> if window is closed.
-	 */
-	public boolean isClosed() {
-		return !this.isVisible();
-	}
+    public GoldMineWindowView()
+    {
+        super();
+    }
 
-	/**
-	 * ========================================================================
-	 * Interface Methods
-	 */
-	/**
-	 * 
-	 */
-	@Override
-	protected void buildContentArea() {
-		this.setTitle("Gold Mine");
-		this.setSize(800, 600);
-		Container mainPane = getContentPane();
-		mainPane.setLayout(new MigLayout("", "12[]6[]"));
-		mainPane.add(buildTable(), "wrap");
-		mainPane.add(buildReadFileButton(), "wrap");
+    /**
+     * @return <code>true</code> if window is closed.
+     */
+    public boolean isClosed()
+    {
+        return !this.isVisible();
+    }
 
-	}
-
-	/**
+    /**
+     * ======================================================================== Interface Methods
+     */
+    /**
 	 * 
 	 */
-	@Override
-	public void clear() {
-		myTable = null;
-		myTradeRecordTableModel = null;
-	}
+    @Override
+    protected void buildContentArea()
+    {
+        this.setTitle( "Gold Mine" );
+        this.setSize( 800, 600 );
+        Container mainPane = getContentPane();
+        mainPane.setLayout( new MigLayout( "", "12[]6[]" ) );
+        mainPane.add( buildTablePanel(), "wrap" );
+        mainPane.add( buildReadFileButton(), "wrap" );
 
-	@Override
-	public void setModel(Object... objs) {
-		@SuppressWarnings("unchecked")
-		List<TradeRecord> trs = (List<TradeRecord>) objs[0];
-		refreshTableModel(trs);
-	}
+    }
 
-	@Override
-	public void tableChanged(TableModelEvent e) {
-		// TODO Auto-generated method stub
-		System.out.println("TableChanged");
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent evt) {
-		if (evt.getSource().equals(myReadFileButton)) {
-			readTradeRecordsFromFile();
-		}
-	}
-
-	/**
-	 * ========================================================================
-	 * Private Methods
+    /**
+	 * 
 	 */
+    @Override
+    public void clear()
+    {
+        myTable = null;
+        myTradeRecordTableModel = null;
+    }
 
-	private JTable buildTable() {
-		myTable = new JTable();
-		myTradeRecordTableModel = new TradeRecordTable(myTable);
-		myTradeRecordTableModel.addTableModelListener(this);
-		myTradeRecordTableModel.addTrade(System.currentTimeMillis(), 19, -330);
-		return myTable;
-	}
+    @Override
+    public void setModel( Object... objs )
+    {
+        @SuppressWarnings( "unchecked" )
+        List<TradeRecord> trs = ( List<TradeRecord> ) objs[0];
+        refreshTableModel( trs );
+    }
 
-	private void refreshTableModel(List<TradeRecord> trs) {
-		if (myTradeRecordTableModel == null) {
-			myTradeRecordTableModel = new TradeRecordTable(myTable);
-		}
-		myTradeRecordTableModel.setDatas(trs);
-	}
+    @Override
+    public void tableChanged( TableModelEvent e )
+    {
+        // TODO Auto-generated method stub
+        System.out.println( "TableChanged" );
+    }
 
-	private JButton buildReadFileButton() {
-		myReadFileButton = new JButton();
-		myReadFileButton.setText("Read Trade Records...");
-		myReadFileButton.addActionListener(this);
-		return myReadFileButton;
-	}
+    @Override
+    public void actionPerformed( ActionEvent evt )
+    {
+        if( evt.getSource().equals( myReadFileButton ) )
+        {
+            readTradeRecordsFromFile();
+        }
+    }
 
-	private void readTradeRecordsFromFile() {
-		JFileChooser chooser = new JFileChooser();
-		FileNameExtensionFilter filter = new FileNameExtensionFilter(
-				"TradeRecords in CSV", "csv");
-		chooser.setFileFilter(filter);
-		int returnVal = chooser.showOpenDialog(this);
-		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			File csvFile = chooser.getSelectedFile();
-			((GoldMineWindowControl) getControl())
-					.readTradeRecordsFromCSVFile(csvFile);
-		}
-	}
+    /**
+     * ======================================================================== Private Methods
+     */
+
+    private JPanel buildTablePanel()
+    {
+        JPanel tablePanel = new JPanel( new MigLayout( "ins 0 0 0 0" ) );
+        myTable = new JTable();
+        myTradeRecordTableModel = new TradeRecordTable( myTable );
+        myTradeRecordTableModel.addTableModelListener( this );
+        myTradeRecordTableModel.addTrade( System.currentTimeMillis(), 19, -330 );
+        tablePanel.add( myTable.getTableHeader(), "wrap" );
+        tablePanel.add( myTable, "wrap" );
+        return tablePanel;
+    }
+
+    private void refreshTableModel( List<TradeRecord> trs )
+    {
+        if( myTradeRecordTableModel == null )
+        {
+            myTradeRecordTableModel = new TradeRecordTable( myTable );
+        }
+        myTradeRecordTableModel.setModel( trs );
+    }
+
+    private JButton buildReadFileButton()
+    {
+        myReadFileButton = new JButton();
+        myReadFileButton.setText( "Read Trade Records..." );
+        myReadFileButton.addActionListener( this );
+        return myReadFileButton;
+    }
+
+    private void readTradeRecordsFromFile()
+    {
+        JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter( "TradeRecords in CSV", "csv" );
+        chooser.setFileFilter( filter );
+        int returnVal = chooser.showOpenDialog( this );
+        if( returnVal == JFileChooser.APPROVE_OPTION )
+        {
+            File csvFile = chooser.getSelectedFile();
+            ( ( GoldMineWindowControl ) getControl() ).readTradeRecordsFromCSVFile( csvFile );
+        }
+    }
 
 }
